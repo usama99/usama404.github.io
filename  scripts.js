428d53055fbe9e295be9ee373278e65aa51a6021
Usama Ejaz <31574039+usama99@users.noreply.github.com>
2024-01-29 16:47:43 +0900

    add homepage

---
+++  scripts.js
@@ -0,0 +1,5 @@
+// Example JavaScript code can go here, such as a script to handle a contact form.
+document.addEventListener('DOMContentLoaded', function() {
+    // DOM is fully loaded, but maybe waiting on images & stylesheets
+    console.log('Hello, world!');
+});
